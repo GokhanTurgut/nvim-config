51f9ea5cee18f0c9ff0c8375688dbab7ad734b53
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-22 02:32:24 +0300

    Add mini indentscope

--- init.lua
+++ init.lua
@@ -303,6 +303,32 @@ require('lazy').setup({
     opts = {},
   },
 
+  {
+    'echasnovski/mini.indentscope',
+    version = '*',
+    event = "VeryLazy",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+
     opts = {
       window = {
         width = 160,
