cf8fdcfeb4ac3d97a68424a7165005ae82b7b8c2
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-21 03:19:16 +0300

    Add mini bufremove

--- init.lua
+++ init.lua
@@ -131,6 +131,34 @@ require('lazy').setup({
     keys = { { "<leader>t", "<cmd>ToggleTerm size=90 direction=vertical<cr>", desc = "Terminal", } }
   },
 
+
+  {
+    "echasnovski/mini.bufremove",
+    version = '*',
+    keys = {
+      {
+        "<leader>d",
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end,
+        desc = "Delete Buffer",
+      },
+      -- stylua: ignore
+      { "<leader>D", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+    },
+  },
+
   {
     "goolord/alpha-nvim",
     event = "VimEnter",
