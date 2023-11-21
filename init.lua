d10b4347448b4eb409b605c2a80ba1f15b855dfd
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-22 02:34:19 +0300

    Add indentscope setup to disable animation

--- init.lua
+++ init.lua
@@ -893,6 +893,13 @@ local servers = {
 -- Setup neovim lua configuration
 require('neodev').setup()
 
+-- Setup indentscope
+require('mini.indentscope').setup({
+  symbol = "│",
+  options = { try_as_border = true },
+  draw = { animation = require('mini.indentscope').gen_animation.none() },
+})
+
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
