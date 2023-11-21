9fd75cd4e81fe7b8b35b3ea9d8e2ab48a4ab38a0
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-21 03:19:47 +0300

    Add bufferline

--- init.lua
+++ init.lua
@@ -131,6 +131,33 @@ require('lazy').setup({
     keys = { { "<leader>t", "<cmd>ToggleTerm size=90 direction=vertical<cr>", desc = "Terminal", } }
   },
 
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>",  desc = "Delete buffers to the right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",   desc = "Delete buffers to the left" },
+      { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",   desc = "Prev buffer" },
+      { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",   desc = "Next buffer" },
+      { "[b",         "<cmd>BufferLineCyclePrev<cr>",   desc = "Prev buffer" },
+      { "]b",         "<cmd>BufferLineCycleNext<cr>",   desc = "Next buffer" },
+    },
+    opts = {
+      options = {
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
 
   {
     "nvim-pack/nvim-spectre",
