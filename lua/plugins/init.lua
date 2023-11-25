922792d1ec67d130e9816d366499409a5501527e
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-25 08:56:23 +0300

    Add plugins lua

---
+++ lua/plugins/init.lua
@@ -0,0 +1,550 @@
+return {
+	-- NOTE: First, some plugins that don't require any configuration
+
+	-- Detect tabstop and shiftwidth automatically
+	'tpope/vim-sleuth',
+
+	-- Git related plugins
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+
+	-- NOTE: This is where your plugins related to LSP can be installed.
+	--  The configuration is done below. Search for lspconfig to find it below.
+	{
+		-- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			'williamboman/mason.nvim',
+			'williamboman/mason-lspconfig.nvim',
+
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ 'j-hui/fidget.nvim', opts = {} },
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+		},
+	},
+
+	{
+		-- Harpoon
+		"ThePrimeagen/harpoon",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			menu = {
+				width = 140,
+				height = 20,
+			},
+		},
+		keys = {
+			{ "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file" },
+			{ "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>",          desc = "Go to next" },
+			{ "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>",          desc = "Go to previous" },
+			{ "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon marks" },
+			{ "<leader>1",  "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>2",  "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>3",  "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>4",  "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>5",  "<cmd>lua require('harpoon.ui').nav_file(5)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>6",  "<cmd>lua require('harpoon.ui').nav_file(6)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>7",  "<cmd>lua require('harpoon.ui').nav_file(7)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>8",  "<cmd>lua require('harpoon.ui').nav_file(8)<cr>",         desc = "which_key_ignore" },
+			{ "<leader>9",  "<cmd>lua require('harpoon.ui').nav_file(9)<cr>",         desc = "which_key_ignore" },
+		},
+	},
+
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("neo-tree.command").execute({ toggle = true })
+				end,
+				desc = "Neotree"
+			},
+		},
+	},
+
+	{
+		'echasnovski/mini.files',
+		version = '*',
+		opts = {
+			options = {
+				-- Whether to delete permanently or move into module-specific trash
+				permanent_delete = false,
+				use_as_default_explorer = false,
+			},
+		},
+		keys = {
+			{
+				"<leader>m",
+				function()
+					require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+				end,
+				desc = "Minifiles"
+			},
+		},
+	},
+
+	{
+		'akinsho/toggleterm.nvim',
+		version = "*",
+		config = true,
+		keys = { { "<leader>t", "<cmd>ToggleTerm size=90 direction=vertical<cr>", desc = "Terminal", } }
+	},
+
+	{
+		'akinsho/bufferline.nvim',
+		version = "*",
+		dependencies = 'nvim-tree/nvim-web-devicons',
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>",  desc = "Delete buffers to the right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",   desc = "Delete buffers to the left" },
+			{ "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",   desc = "Prev buffer" },
+			{ "<S-l>",      "<cmd>BufferLineCycleNext<cr>",   desc = "Next buffer" },
+			{ "[b",         "<cmd>BufferLineCyclePrev<cr>",   desc = "Prev buffer" },
+			{ "]b",         "<cmd>BufferLineCycleNext<cr>",   desc = "Next buffer" },
+		},
+		opts = {
+			options = {
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+			},
+		},
+	},
+
+	{
+		"nvim-pack/nvim-spectre",
+		keys = {
+			{ "<leader>R", function() require("spectre").open() end, desc = "Replace (Spectre)" },
+		},
+		opts = {
+			mapping = {
+				['send_to_qf'] = {
+					map = "<leader>f",
+					cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+					desc = "send all items to quickfix"
+				},
+			},
+		},
+	},
+
+	{
+		"echasnovski/mini.bufremove",
+		version = '*',
+		keys = {
+			{
+				"<leader>d",
+				function()
+					local bd = require("mini.bufremove").delete
+					if vim.bo.modified then
+						local choice = vim.fn.confirm(
+							("Save changes to %q?"):format(vim.fn.bufname()),
+							"&Yes\n&No\n&Cancel")
+						if choice == 1 then -- Yes
+							vim.cmd.write()
+							bd(0)
+						elseif choice == 2 then -- No
+							bd(0, true)
+						end
+					else
+						bd(0)
+					end
+				end,
+				desc = "Delete Buffer",
+			},
+			-- stylua: ignore
+			{ "<leader>D", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+		},
+	},
+
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		enabled = true,
+		init = false,
+		opts = function()
+			local dashboard = require("alpha.themes.dashboard")
+			local logo = [[
+╔───────────────────────────────────────────────────────────────────────╗
+│                                                                       │
+│                                                                       │
+│                                                                       │
+│      ██████╗  ██████╗ ██╗  ██╗██╗  ██╗ █████╗ ███╗   ██╗████████╗     │
+│     ██╔════╝ ██╔═══██╗██║ ██╔╝██║  ██║██╔══██╗████╗  ██║╚══██╔══╝     │
+│     ██║  ███╗██║   ██║█████╔╝ ███████║███████║██╔██╗ ██║   ██║        │
+│     ██║   ██║██║   ██║██╔═██╗ ██╔══██║██╔══██║██║╚██╗██║   ██║        │
+│     ╚██████╔╝╚██████╔╝██║  ██╗██║  ██║██║  ██║██║ ╚████║   ██║        │
+│      ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝        │
+│                                                                       │
+│                                                                       │
+│                                                                       │
+╚───────────────────────────────────────────────────────────────────────╝
+    ]]
+
+			dashboard.section.header.val = vim.split(logo, "\n")
+			-- stylua: ignore
+			dashboard.section.buttons.val = {
+				dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+				dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+				dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+				dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+				dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+				dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+			}
+			for _, button in ipairs(dashboard.section.buttons.val) do
+				button.opts.hl = "AlphaButtons"
+				button.opts.hl_shortcut = "AlphaShortcut"
+			end
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.section.buttons.opts.hl = "AlphaButtons"
+			dashboard.section.footer.opts.hl = "AlphaFooter"
+			dashboard.opts.layout[1].val = 8
+			return dashboard
+		end,
+		config = function(_, dashboard)
+			-- close Lazy and re-open when the dashboard is ready
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					once = true,
+					pattern = "AlphaReady",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
+
+			require("alpha").setup(dashboard.opts)
+
+			vim.api.nvim_create_autocmd("User", {
+				once = true,
+				pattern = "LazyVimStarted",
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					dashboard.section.footer.val = "⚡ Neovim loaded "
+					    .. stats.loaded
+					    .. "/"
+					    .. stats.count
+					    .. " plugins in "
+					    .. ms
+					    .. "ms"
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
+		end,
+	},
+
+	{
+		"shortcuts/no-neck-pain.nvim",
+		version = "*",
+		opts = {
+			width = 160,
+		},
+		keys = {
+			{ "<leader>z", "<cmd>NoNeckPain<cr>", desc = "NoNeckPain" },
+		}
+	},
+
+	{
+		'echasnovski/mini.surround',
+		version = '*',
+		event = "VeryLazy",
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+			},
+		}
+	},
+
+	{
+		'echasnovski/mini.pairs',
+		version = '*',
+		opts = {},
+	},
+
+	{
+		'echasnovski/mini.indentscope',
+		version = '*',
+		event = "VeryLazy",
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		event = "VeryLazy",
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+			},
+		},
+	},
+
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		-- stylua: ignore
+		keys = {
+			{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+			{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+			{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+		},
+	},
+
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		opts = {},
+	},
+
+	{
+		-- Autocompletion
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+
+			-- Adds path completion
+			"hrsh7th/cmp-path",
+
+			-- Adds copilot support
+			{
+				"zbirenbaum/copilot-cmp",
+				dependencies = "copilot.lua",
+				opts = {},
+				config = function(_, opts)
+					local copilot_cmp = require("copilot_cmp")
+					copilot_cmp.setup(opts)
+					-- attach cmp source whenever copilot attaches
+					-- fixes lazy-loading issues with the copilot cmp source
+					local on_attach = function(on_attach_func)
+						vim.api.nvim_create_autocmd("LspAttach", {
+							callback = function(args)
+								local buffer = args.buf ---@type number
+								local client = vim.lsp.get_client_by_id(args.data
+									.client_id)
+								on_attach_func(client, buffer)
+							end,
+						})
+					end
+					on_attach(function(client)
+						if client.name == "copilot" then
+							copilot_cmp._on_insert_enter({})
+						end
+					end)
+				end,
+			},
+		},
+	},
+
+	-- Useful plugin to show you pending keybinds.
+	{ 'folke/which-key.nvim',  opts = {} },
+
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				map('n', '<leader>gb', function() gs.blame_line { full = true } end,
+					{ desc = 'Blame line' })
+				map('n', '<leader>gB', gs.toggle_current_line_blame, { desc = 'Toggle blame line' })
+			end,
+		},
+	},
+
+	{
+		-- Theme inspired by Atom
+		'catppuccin/nvim',
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme 'catppuccin-mocha'
+		end,
+		opts = {
+			integrations = {
+				aerial = true,
+				alpha = true,
+				cmp = true,
+				dashboard = true,
+				flash = true,
+				gitsigns = true,
+				headlines = true,
+				illuminate = true,
+				indent_blankline = { enabled = true },
+				leap = true,
+				lsp_trouble = true,
+				mason = true,
+				markdown = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				navic = { enabled = true, custom_bg = "lualine" },
+				neotest = true,
+				neotree = true,
+				noice = true,
+				notify = true,
+				semantic_tokens = true,
+				telescope = true,
+				treesitter = true,
+				treesitter_context = true,
+				which_key = true,
+			},
+		},
+	},
+
+	{
+		-- Set lualine as statusline
+		'nvim-lualine/lualine.nvim',
+		-- See `:help lualine.txt`
+		event = "VeryLazy",
+		opts = {
+			globalstatus = true,
+		},
+	},
+
+	{
+		-- Add indentation guides even on blank lines
+		'lukas-reineke/indent-blankline.nvim',
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = 'ibl',
+		opts = {},
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim', opts = {} },
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+			-- Only load if `make` is available. Make sure you have the system
+			-- requirements installed.
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = 'make',
+				cond = function()
+					return vim.fn.executable 'make' == 1
+				end,
+			},
+		},
+	},
+
+	{
+		-- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ':TSUpdate',
+	},
+
+}
