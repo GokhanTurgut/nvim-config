6a3f7548d596a6884e68e00b074055bfe65f28e7
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-27 02:49:50 +0300

    Add conform nvim for formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+  "stevearc/conform.nvim",
+  lazy = false,
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        css = { { "prettierd", "prettier" } },
+        html = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        yaml = { { "prettierd", "prettier" } },
+        markdown = { { "prettierd", "prettier" } },
+        graphql = { { "prettierd", "prettier" } },
+        lua = { "stylua" },
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+      conform.format({
+        lsp_fallback = true,
+        timeout_ms = 3000,
+      })
+    end, { desc = "Format" })
+  end,
+}
