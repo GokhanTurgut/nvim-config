4762584b1293e86563627f9b81745febf4e4ab42
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-12-05 04:02:33 +0300

    Add tmux navigation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -470,4 +470,20 @@ return {
     },
     build = ":TSUpdate",
   },
+
+  {
+    "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+      nvim_tmux_nav.setup({
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+        },
+      })
+    end,
+  },
 }
