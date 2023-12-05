d69141ab0119db854ead4a10682a0de5d4f00145
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-12-05 03:54:07 +0300

    Update cmp setup

--- init.lua
+++ init.lua
@@ -329,37 +329,10 @@ cmp.setup({
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete({}),
     ["<C-l>"] = cmp.mapping.confirm({ select = true }),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
   }),
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
-    {
-      name = "copilot",
-      group_index = 1,
-      priority = 100,
-    },
   },
 })
 
