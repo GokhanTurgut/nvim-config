d0e5e44a6c2656ac0608dc25e559412df181eafc
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-12-15 23:25:25 +0300

    Remove unused buffer keymaps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,8 +127,6 @@ return {
     event = "VeryLazy",
     keys = {
       { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
-      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
       { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
       { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
       { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
