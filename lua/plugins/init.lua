dbcef74a3c0acb3f6601c9d617e8e17cc6b15a37
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-12-26 15:52:06 +0300

    Refactor treesitter setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -395,6 +395,27 @@ return {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
     build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = {
+          "go",
+          "lua",
+          "python",
+          "rust",
+          "tsx",
+          "javascript",
+          "typescript",
+          "vimdoc",
+          "vim",
+          "bash",
+          "ruby",
+        },
+
+        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        auto_install = true,
+      })
+    end,
   },
 
   {
