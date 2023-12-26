56c08b464e8202510d71ce38b1d0011d1dd0d6c6
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-12-26 16:11:12 +0300

    Fix treesitter loading issue

--- init.lua
+++ init.lua
@@ -48,6 +48,31 @@ require("lazy").setup("plugins", {
   },
 })
 
+-- [[ Configure Treesitter ]]
+-- See `:help nvim-treesitter`
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+  require("nvim-treesitter.configs").setup({
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = {
+      "go",
+      "lua",
+      "python",
+      "rust",
+      "tsx",
+      "javascript",
+      "typescript",
+      "vimdoc",
+      "vim",
+      "bash",
+      "ruby",
+    },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  })
+end, 0)
+
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
