cde092299f5cfb604ede44d149bd85565f039801
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-12-26 15:52:37 +0300

    Refactor mason and lspconfig setup

--- init.lua
+++ init.lua
@@ -109,28 +109,6 @@ require("which-key").register({
   ["<leader>t"] = { name = "Terminal", _ = "which_key_ignore" },
 })
 
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
-require("mason").setup()
-require("mason-lspconfig").setup()
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
-local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-}
-
 -- Setup neovim lua configuration
 require("neodev").setup()
 
@@ -140,30 +118,26 @@ require("mini.indentscope").setup({
   options = { try_as_border = true },
   draw = { animation = require("mini.indentscope").gen_animation.none() },
 })
+require("mason").setup()
+require("mason-lspconfig").setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
--- Ensure the servers above are installed
-local mason_lspconfig = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
 
-mason_lspconfig.setup({
-  ensure_installed = vim.tbl_keys(servers),
-})
-
-mason_lspconfig.setup_handlers({
+require("mason-lspconfig").setup_handlers({
   function(server_name)
     require("lspconfig")[server_name].setup({
       capabilities = capabilities,
       on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
     })
   end,
 })
 
-require("lspconfig").rubocop.setup({
+-- Setup manually installed LSPs
+lspconfig.rubocop.setup({
   capabilities = capabilities,
   on_attach = on_attach,
   filetypes = { "ruby" },
@@ -171,7 +145,7 @@ require("lspconfig").rubocop.setup({
   root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
 })
 
-require("lspconfig").sorbet.setup({
+lspconfig.sorbet.setup({
   capabilities = capabilities,
   on_attach = on_attach,
   filetypes = { "ruby" },
