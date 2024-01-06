b33b0b7abf97f5c9734551caa1395afc470d61c9
Gokhan Turgut <gokhan.turgut@gusto.com>
2024-01-06 22:47:35 +0300

    Add gitsings plugin file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -183,36 +183,6 @@ return {
   { "folke/which-key.nvim", opts = {} },
 
   {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = "│" },
-        change = { text = "│" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-        untracked = { text = "┆" },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map("n", "<leader>gb", function()
-          gs.blame_line({ full = true })
-        end, { desc = "Blame line" })
-        map("n", "<leader>gB", gs.toggle_current_line_blame, { desc = "Toggle blame line" })
-      end,
-    },
-  },
-
-  {
     -- Theme inspired by Atom
     "catppuccin/nvim",
     lazy = false,
