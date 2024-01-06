1de2e9247f42890fce2fd35dde1583137771eba3
Gokhan Turgut <gokhan.turgut@gusto.com>
2024-01-06 22:46:55 +0300

    Add bufferline plugin file

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,25 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+  },
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "NvimTree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    },
+  },
+}
