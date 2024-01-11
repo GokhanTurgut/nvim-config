cc583f5766e0e4e787834cf1d5f7d8f679c0b58f
Gokhan Turgut <gokhan.turgut@gusto.com>
2024-01-11 05:52:15 +0300

    Add mini plugins file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,57 +10,6 @@ return {
   },
 
   {
-    "echasnovski/mini.files",
-    version = "*",
-    opts = {
-      options = {
-        -- Whether to delete permanently or move into module-specific trash
-        permanent_delete = false,
-      },
-      windows = {
-        -- Width of focused window
-        width_focus = 60,
-        -- Width of non-focused window
-        width_nofocus = 20,
-      },
-    },
-    keys = {
-      {
-        "<leader>m",
-        function()
-          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-        end,
-        desc = "Minifiles",
-      },
-    },
-  },
-
-  {
-    "echasnovski/mini.bufremove",
-    version = "*",
-    keys = {
-      {
-        "<leader>d",
-        function()
-          local bd = require("mini.bufremove").delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-            if choice == 1 then -- Yes
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then -- No
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-        desc = "Delete buffer",
-      },
-    },
-  },
-
-  {
     "shortcuts/no-neck-pain.nvim",
     version = "*",
     opts = {
@@ -69,62 +18,6 @@ return {
     keys = {
       { "<leader>z", "<cmd>NoNeckPain<cr>", desc = "NoNeckPain" },
     },
-  },
-
-  {
-    "echasnovski/mini.surround",
-    version = "*",
-    event = "VeryLazy",
-    opts = {
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
-  },
-
-  {
-    "echasnovski/mini.pairs",
-    version = "*",
-    opts = {},
-  },
-
-  {
-    "echasnovski/mini.indentscope",
-    version = "*",
-    event = "VeryLazy",
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "help",
-          "alpha",
-          "dashboard",
-          "nvim-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
-    config = function()
-      require("mini.indentscope").setup({
-        symbol = "│",
-        options = { try_as_border = true },
-        draw = { animation = require("mini.indentscope").gen_animation.none() },
-      })
-    end,
   },
 
   {
