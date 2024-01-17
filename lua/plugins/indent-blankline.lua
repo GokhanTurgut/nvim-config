d09140ce47df55e7fd2be90b58c108040308f69f
Gokhan Turgut <gokhan.turgut@gusto.com>
2024-01-17 20:17:12 +0300

    Add indent blankline plugin file

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,27 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  event = "VeryLazy",
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "nvim-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,35 +21,7 @@ return {
   },
 
   {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = "VeryLazy",
-    opts = {
-      indent = {
-        char = "│",
-        tab_char = "│",
-      },
-      scope = { enabled = false },
-      exclude = {
-        filetypes = {
-          "help",
-          "alpha",
-          "dashboard",
-          "nvim-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-      },
-    },
-  },
-
-  {
-    -- Theme inspired by Atom
+    -- Theme
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
