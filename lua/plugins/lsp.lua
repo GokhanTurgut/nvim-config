553876a7fdabfd2a0af25c9c4fa6984ea8cbf952
Gokhan Turgut <gokhan.turgut@gusto.com>
2024-09-19 12:08:00 +0300

    Update to ts_ls due to deprecation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -107,7 +107,7 @@ return {
       --   single_file_support = true,
       -- })
 
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities,
         on_attach = on_attach,
         filetypes = {
