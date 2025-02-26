fe82ad208070f2879054c63a1b5c6b2b7886d649
Gokhan Turgut <gokhan.turgut@gusto.com>
2025-02-26 16:24:28 +0300

    Fix conform for formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,16 +6,16 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        css = { { "prettierd", "prettier" } },
-        html = { { "prettierd", "prettier" } },
-        json = { { "prettierd", "prettier" } },
-        yaml = { { "prettierd", "prettier" } },
-        markdown = { { "prettierd", "prettier" } },
-        graphql = { { "prettierd", "prettier" } },
+        javascript = { "prettierd", "prettier" },
+        typescript = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        css = { "prettierd", "prettier" },
+        html = { "prettierd", "prettier" },
+        json = { "prettierd", "prettier" },
+        yaml = { "prettierd", "prettier" },
+        markdown = { "prettierd", "prettier" },
+        graphql = { "prettierd", "prettier" },
         lua = { "stylua" },
       },
     })
